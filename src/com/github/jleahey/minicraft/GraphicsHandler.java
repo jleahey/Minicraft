82be4a18a98621df6e3d7fdeaebbab0db3ec3e05
Jonny Leahey <jleaheycs@gmail.com>
2012-12-19 16:57:26 -0500

    Adding generic graphics for extending in other systems
    Thinking about Andoird!

---
+++ src/com/github/jleahey/minicraft/GraphicsHandler.java
@@ -0,0 +1,6 @@
+package com.github.jleahey.minicraft;
+
+public abstract class GraphicsHandler {
+	
+	public abstract void init(int width, int height);
+}
--- src/com/github/jleahey/minicraft/Sprite.java
+++ src/com/github/jleahey/minicraft/Sprite.java
@@ -6,50 +6,23 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-/**
- * A sprite to be displayed on the screen. Note that a sprite
- * contains no state information, i.e. its just the image and
- * not the location. This allows us to use a single sprite in
- * lots of different places without having to store multiple
- * copies of the image.
- * 
- * @author Kevin Glass
- */
-public class Sprite implements java.io.Serializable {
-	private static final long serialVersionUID = 1L;
+public interface Sprite extends java.io.Serializable {
 	
-	/** The image to be drawn for this sprite */
-	transient private Image image;
-	private String ref;
-	
-	/**
-	 * Create a new sprite based on an image
-	 * 
-	 * @param image
-	 *            The image that is this sprite
-	 */
-	public Sprite(Image image, String ref) {
-		this.image = image;
-		this.ref = ref;
-	}
+	public void set(Image image, String ref);
 	
 	/**
 	 * Get the width of the drawn sprite
 	 * 
 	 * @return The width in pixels of this sprite
 	 */
-	public int getWidth() {
-		return image.getWidth(null);
-	}
+	public int getWidth() ;
 	
 	/**
 	 * Get the height of the drawn sprite
 	 * 
 	 * @return The height in pixels of this sprite
 	 */
-	public int getHeight() {
-		return image.getHeight(null);
-	}
+	public int getHeight() ;
 	
 	/**
 	 * Draw the sprite onto the graphics context provided
@@ -61,34 +34,12 @@ public class Sprite implements java.io.Serializable {
 	 * @param y
 	 *            The y location at which to draw the sprite
 	 */
-	public void draw(Graphics g, int x, int y) {
-		g.drawImage(image, x, y, null);
-	}
+	public void draw(Graphics g, int x, int y) ;
 	
-	public void draw(Graphics g, int x, int y, int width, int height) {
-		g.drawImage(image, x, y, width, height, null);
-	}
+	public void draw(Graphics g, int x, int y, int width, int height);
+
+	void readObject(ObjectInputStream aInputStream) throws ClassNotFoundException,
+			IOException;
 	
-	/**
-	 * Always treat de-serialization as a full-blown constructor, by
-	 * validating the final state of the de-serialized object.
-	 */
-	private void readObject(ObjectInputStream aInputStream) throws ClassNotFoundException,
-			IOException {
-		// always perform the default de-serialization first
-		// aInputStream.defaultReadObject();
-		ref = (String) aInputStream.readObject();
-		this.image = SpriteStore.get().getSprite(ref).image;
-	}
-	
-	/**
-	 * This is the default implementation of writeObject.
-	 * Customise if necessary.
-	 */
-	private void writeObject(ObjectOutputStream aOutputStream) throws IOException {
-		// perform the default serialization for all non-transient, non-static fields
-		aOutputStream.writeObject(ref);
-		aOutputStream.defaultWriteObject();
-	}
-	
-}
+	void writeObject(ObjectOutputStream aOutputStream) throws IOException;
+};
